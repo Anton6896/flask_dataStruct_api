b6e12cb7b46066672de219ac01ea8d90d8aceb02
Ant_Fedora <Anton-r@live.com>
2021-04-24 10:21:12 +0300

    db ready (tst)

--- server.py
+++ server.py
@@ -23,4 +23,20 @@ db = SQLAlchemy(app)
 now = datetime.now()
 
 
-# requirements.txt
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(50))
+    posts = db.relationship("BlogPost", cascade="all, delete")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))  # for testing purpose
+    date = db.column(db.DATE)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
