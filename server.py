16364ec997f49af873216134971caf21f168ed81
Ant_Fedora <Anton-r@live.com>
2021-04-24 11:07:05 +0300

    with the databae (its very small )

--- server.py
+++ server.py
@@ -4,15 +4,17 @@ from datetime import datetime
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
 from flask_sqlalchemy import SQLAlchemy
+from . import my_routes
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
-app.config["SQL_TRACK_MODIFICATIONS"] = 0
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 
 
 # configure sqlite3 to enforce foreign key constraints
 @event.listens_for(Engine, "connect")
 def _set_sqlite_pragma(dbapi_connection, connection_record):
+    # force the blog_post to check appropriate user.id at user table
     if isinstance(dbapi_connection, SQLite3Connection):
         cursor = dbapi_connection.cursor()
         cursor.execute("PRAGMA foreign_keys=ON;")
@@ -20,6 +22,7 @@ def _set_sqlite_pragma(dbapi_connection, connection_record):
 
 
 db = SQLAlchemy(app)
+# dont forget >>> db.create_all()
 now = datetime.now()
 
 
@@ -38,5 +41,7 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     body = db.Column(db.String(200))  # for testing purpose
-    date = db.column(db.DATE)
+    date = db.Column(db.Date)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+# all routes was transferred to my_routes file
