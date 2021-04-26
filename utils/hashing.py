3ff8632e327f85c4fada16c801477462ab4e18fb
Ant_Fedora <Anton-r@live.com>
2021-04-26 08:57:11 +0300

    Hashing

--- utils/hashing.py
+++ utils/hashing.py
@@ -16,12 +16,13 @@ class HashTable:
         self.hash_table = [None] * self.table_size
 
     def custom_hash(self, key) -> int:
-        # hashing function implementation (for an example only)!! this func is full in collisions
+        # hashing function implementation (for an example only)!!
+        # this func is full in collisions
         hash_value = 0
         for i in key:
             hash_value += ord(i)  # asii repr
             # modulo insure that size never bigger than table
-            hash_value = (hash_value * ord(i)) % self.table_size
+            hash_value = (hash_value ** 2) % self.table_size
         return int(hash_value)
 
     def add_key_value(self, key, value):
@@ -30,4 +31,40 @@ class HashTable:
             self.hash_table[hashed_key] = Node(Data(key, value))
 
         else:  # there is collision -> add ti linked list new Node with Data
-            self.hash_table[hashed_key].next_node(Node(Data(key, value)))
+            node = self.hash_table[hashed_key]
+            while node.next_node:  # traverse to eng
+                node = node.next_node
+            node.next_node = Node(Data(key, value))
+
+    def get_value(self, key):
+        hashed_key = self.custom_hash(key)
+
+        if self.hash_table[hashed_key]:
+            node = self.hash_table[hashed_key]
+            while node:
+                if node.data.key == key:
+                    return node.data.value
+                node = node.next_node
+
+        return None
+
+    def print(self):
+        for i, val in enumerate(self.hash_table):
+            if val:
+                print(f" place {i} ---")
+                node = val
+                while node:
+                    print(
+                        f"[ key:{node.data.key},"
+                        f" hashed_key:{self.custom_hash(node.data.key)},"
+                        f" value:{node.data.value} ] ")
+                    node = node.next_node
+                print("==============================")
+
+
+if __name__ == '__main__':
+    hs = HashTable(4)
+    hs.add_key_value("aa", "aa")
+    hs.add_key_value("bb", "bb")
+    hs.add_key_value('7', '4')
+    hs.print()
