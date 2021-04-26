05213ab29da5b95dd9ddf361370a4ea6cda6b485
Ant_Fedora <Anton-r@live.com>
2021-04-26 10:48:24 +0300

    Hashing

--- utils/bst.py
+++ utils/bst.py
@@ -9,19 +9,42 @@ class BST:
     def __init__(self):
         self.root = None
 
-    def _insert(self, value, node):
-        if not node:
-            node = Node(value)
-
-        # small will be left other right
-        # let assume that data is comparable obj (if value same -> do nothing)
-        if node.data < value:
-            self._insert(value, node.left)
+    def _insert(self, value, node: Node):
+        if value < node.data:
+            if node.left is None:
+                node.left = Node(value)
+                return
+            else:
+                self._insert(value, node.left)
+        elif value > node.data:
+            if node.right is None:
+                node.right = Node(value)
+                return
+            else:
+                self._insert(value, node.right)
         else:
-            self._insert(value, node.right)
+            return
 
     def insert(self, value):
         if not self.root:
             self.root = Node(value)
         else:
             self._insert(value, self.root)
+
+        return self
+
+    def _print_inorder(self, root):
+        if root:
+            self._print_inorder(root.left)
+            print(root.data, end=" - ")
+            self._print_inorder(root.right)
+
+    def print(self):
+        return self._print_inorder(self.root)
+
+
+if __name__ == '__main__':
+    bs = BST()
+    bs.insert(2)
+    bs.insert(1)
+    bs.print()
